39d7f533471b996a5832f03d623bd27564cc8c31
Ryan McCahon <mccahonr@yahoo.com>
2019-08-14 16:21:43 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+	if speed > 60
+		response = true
+	elsif speed < 40
+		response = true
+	else
+		response = false
+	end
+	response
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
 end
-	
-
-
